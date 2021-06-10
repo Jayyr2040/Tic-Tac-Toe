e7dee1dd3e934486e8e94d68448a4139d289da01
jaytai2040 <jayyr2040@gmail.com>
2021-06-10 17:07:56 +0800

    Check if a square was already clicked

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+let isItX = true;
+
+const isIt = (event) => {
+    const $target = $(event.currentTarget);
+    if (isItX === true) {
+        $target.text("X");
+        isItX = false;
+        }
+        else if (isItX === false){
+        $target.text("O");
+        isItX = true;
+        }
+}
+
+const isFilled = (event) => {
+    const $target = $(event.currentTarget);
+    if ($target.text() !== "") {
+        }
+    else if ($target.text() === ""){
+        isIt(event);
+        }
+}
+
+const printX = (event, isItX) => {
+    const $target = $(event.currentTarget);
+    isFilled(event);
+    //isIt(event);
+    $target.css("font-size","90px");
+    $target.css("text-align","center");
+    $target.css("display","flex");
+    $target.css("align-items","center");
+    $target.css("justify-content","center");
+    $target.css("flex-direction","column");
+    $target.css("overflow","hidden");
+    
+
+}
+
 const render = () => {
     const $body = $('body');
     for (let i = 10; i < 13; i++){
@@ -5,10 +43,12 @@ const render = () => {
         $body.append($div.addClass("gameBoard").attr("id",`${i}`));
         for (let j = 1; j < 4; j++){
             const $div = $('<div>');
-            $(`#${i}`).append($div.addClass("gameBox").on("click", () => {console.log("hi")}));
+            $(`#${i}`).append($div.addClass("gameBox").on("click", printX));
     }
 }
 }
+
+
 
 const main = () => {
 render();
